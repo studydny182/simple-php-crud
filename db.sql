a2352ac4aeb97b3089bed540ce72c6d12e92fa84
studydny182 <study.dny182@gmail.com>
2026-02-21 12:00:30 +0700

    Update db.sql

--- db.sql
+++ db.sql
@@ -27,7 +27,7 @@ CREATE TABLE `users` (
   `name` varchar(100) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
